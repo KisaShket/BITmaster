a4187ef43a02fac6ebb4d082d948ca86f513b8e2
Miska <nik55-55@mail.ru>
2019-11-03 15:05:37 +0400

    Initial Commit

---
+++ BIT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BIT
+//
+//  Created by Miska  on 03/11/2019.
+//  Copyright © 2019 Miska . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
